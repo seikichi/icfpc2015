27722b7a70148a551286c036f3ef0fe7c700cfbc
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-08 02:48:05 +0900

    AIの枠作成

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,74 @@
+#include "ai.h"
 #include "game.h"
 
-int main() {
+#include <cstdio>
+#include <unistd.h>
+
+#include <fstream>
+#include <memory>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <vector>
+
+#include "picojson/picojson.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+  // input
+  vector<string> problem_files;
+  int time_limit_seconds;
+  int memory_limit;
+  vector<string> phrases_of_power;
+
+  int result;
+  while ((result = getopt(argc, argv, "f:t:m:p:")) != -1) {
+    switch (result) {
+    case 'f':
+      problem_files.push_back(optarg);
+      break;
+    case 't':
+      time_limit_seconds = stoi(optarg);
+      break;
+    case 'm':
+      memory_limit = stoi(optarg);
+      break;
+    case 'p':
+      phrases_of_power.push_back(optarg);
+      break;
+    default:
+      break;
+    }
+  }
+
+  // process
+  stringstream ss;
+  ss << "[";
+  bool first = true;
+  for (const auto& problem_file : problem_files) {
+    Game game;
+    auto source_seed_idx = 0;
+
+    ifstream ifs(problem_file.c_str());
+    string problem((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());;
+    while (game.Init(problem, source_seed_idx++)) {
+      auto ai = make_shared<AI>();
+      ai->Init();
+
+      if (!first) { ss << ","; }
+      auto solution = ai->Run(game);
+      ss << "{";
+      ss << "\"problemId\": " << game.problem_id << ", ";
+      ss << "\"seed\": " << game.source_seed << ", ";
+      ss << "\"tag\": " << "\"kyoto ni modoritai\"" << ", ";
+      ss << "\"solution\": " << "\"" << solution << "\"";
+      ss << "}";
+      first = false;
+    }
+  }
+  ss << "]";
+
+  // output
+  cout << ss.str() << endl;
   return 0;
 }
