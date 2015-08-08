9242bb89737eb1add6221ff6ae3d306710881da1
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-08 16:53:57 +0900

    update kichi AI

--- src/kichiai.cc
+++ src/kichiai.cc
@@ -4,8 +4,6 @@
 #include <queue>
 using namespace std;
 
-// MOVE, LOCK, ERROR
-
 namespace {
 
 struct SearchState {
@@ -15,59 +13,55 @@ struct SearchState {
     : state(state), commands(commands) {}
 };
 
-string Step(const Game& game, const State& initial_state) {
-  // game.CurrentUnit(state.source_idx);
-  vector<char> commands = {'p', 'b', 'a', 'l', 'd', 'k'};
-  queue<SearchState> que;
-  string step_result = "";
-  double max_score = -1;
-  vector<bool> visited(game.h * game.w * 6, false);
-  que.push(SearchState(initial_state, ""));
-
-  while (!que.empty()) {
-    SearchState search_state = que.front(); que.pop();
-    const State& state = search_state.state;
-
-    int visited_index = game.w * 6 * state.pivot.y +
-      6 * state.pivot.x + state.rot;
-
-    if (visited[visited_index]) { continue; }
-    visited[visited_index] = true;
-
-    for (auto c : commands) {
-      State next_state = search_state.state;
-      CommandResult result = next_state.Command(game, c);
-      string next_commands = search_state.commands + string(1, c);
-      if (result == ERROR || result == GAMEOVER) { continue; }
-      if (result == LOCK || result == CLEAR) {
-        double score = state.pivot.y;
-        if (score > max_score) {
-          max_score = score;
-          step_result = next_commands;
-        }
-        continue;
-      }
-
-      que.push(SearchState(next_state, next_commands));
-    }
-  }
-
-  return step_result;
-}
-
-// pivot.{y,x} , rot % period (max 0~5)
-// 
 };
 
 string KichiAI::Run(const Game& game) {
+  const vector<char> commands = {'p', 'b', 'a', 'l', 'd', 'k'};
   State state;
   state.Init(game);
 
   stringstream solution;
+
   while (true) {
-    string next = Step(game, state);
-    if (next == "") { break; }
-    for (const char c : next) {
+    // 1ユニットごとにループ
+    State initial_state = state;
+    vector<bool> visited(game.h * game.w * 6, false);
+    queue<SearchState> que;
+    string step_result = "";
+    double max_score = -1;
+    que.push(SearchState(initial_state, ""));
+
+    while (!que.empty()) {
+      const SearchState search_state = que.front();
+      que.pop();
+      const State& state = search_state.state;
+
+      const int visited_index = game.w * 6 * state.pivot.y +
+        6 * state.pivot.x + state.rot;
+
+      if (visited[visited_index]) { continue; }
+      visited[visited_index] = true;
+
+      for (auto c : commands) {
+        State next_state = search_state.state;
+        CommandResult result = next_state.Command(game, c);
+        string next_commands = search_state.commands + string(1, c);
+        if (result == ERROR || result == GAMEOVER) { continue; }
+        if (result == LOCK || result == CLEAR) {
+          double score = state.pivot.y;
+          if (score > max_score) {
+            max_score = score;
+            step_result = next_commands;
+          }
+          continue;
+        }
+
+        que.push(SearchState(next_state, next_commands));
+      }
+    }
+
+    if (step_result == "") { return solution.str(); }
+    for (const char c : step_result) {
       CommandResult result = state.Command(game, c);
       if (result == ERROR || result == GAMEOVER) {
         return solution.str();
@@ -79,5 +73,6 @@ string KichiAI::Run(const Game& game) {
       solution << c;
     }
   }
-  return solution.str();
+
+  return "";
 }
