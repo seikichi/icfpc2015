a6f4584e0485fed101751a32a7f5f065631bda37
Yusuke Nojima <nojima@ynojima.com>
2015-08-10 01:34:36 +0900

    Merge ronricoai and submarineai.

--- src/submarineai.cc
+++ src/submarineai.cc
@@ -1,10 +1,12 @@
 #include "submarineai.h"
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <queue>
 #include <cassert>
 #include <cmath>
 #include <sys/time.h>
+#include <set>
 using namespace std;
 
 namespace {
@@ -45,17 +47,155 @@ struct ItemAnnealing {
 //   return state.pivot.y;
 // }
 
-int evaluateScore(const Game&, const State& state, const State& next_state) {
-  return state.pivot.y + next_state.score;
-  // return state.pivot.y;
-}
-
 long long getTime() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return (long long)tv.tv_sec * 1000000 + (long long)tv.tv_usec;
 }
 
+int calculateDistanceFromCenter(const Game& game, const vector<pair<int,int> >& current_cell_positions) {
+  // units should go to both sides (distance from center)
+  int average_x = 0;
+  for (const auto& position : current_cell_positions) {
+    average_x += position.first;
+  }
+  return fabs((game.w / 2) - (int)(average_x / current_cell_positions.size()));
+}
+
+bool isWallOrFilled(const Game& game, const State& state, const pair<int,int>& position) {
+  if (position.first < 0 || position.first >= game.w || position.second < 0 || position.second >= game.h){
+    return true;
+  } else if (state.board[Cell(position.first, position.second).Lin(game.w)]){ 
+    return true;
+  }
+  return false;
+}
+
+int isInUnit(const vector<pair<int,int> >& current_cell_positions, const pair<int,int>& position) {
+  for (auto& cell_position : current_cell_positions) {
+    if (position == cell_position) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// if the result makes hole whose size is within "threshold", get the score "penalty"
+double avoidHole(const Game& game, 
+              const State& state,
+              const vector<pair<int,int> >& current_cell_positions,
+              const vector<pair<int,int> >& neighbors,
+              const vector<vector<pair<int,int> > >& directions,
+              const double penalty,
+              const double normal,
+              const int threshold) {
+  // how many cells from neighbor? (bfs)
+  for (const auto& neighbor : neighbors) {
+    set<pair<int,int> > not_filled_cell_positions;
+    queue<pair<int,int> > que;
+
+    if (isInUnit(current_cell_positions, neighbor) || isWallOrFilled(game, state, neighbor)) {
+      continue;
+    }
+    que.push(neighbor);
+
+    while (!que.empty() && (int)not_filled_cell_positions.size() <= threshold) {
+      auto& target = que.front(); que.pop();
+      not_filled_cell_positions.insert(target);
+
+      for(const auto& direction : directions[target.second % 2]) {
+        pair<int, int> next(target.first + direction.first, target.second + direction.second);
+        if (not_filled_cell_positions.count(next) == 1) {
+          continue;
+        }
+        if (!isInUnit(current_cell_positions, next) && !isWallOrFilled(game, state, next)) {
+          que.push(next);
+        }
+      }
+    }
+    if ((int)not_filled_cell_positions.size() <= threshold) {
+      return penalty;
+    }
+  }
+  return normal;
+}
+
+
+int calculateFilledNeighbors(const Game& game, const State& state, const vector<pair<int,int> >& neighbors) {
+  int filled_neighbor = 0;
+  for(const auto& neighbor : neighbors) {
+      if (neighbor.first < 0 || neighbor.first >= game.w || neighbor.second >= game.h){
+        ++filled_neighbor;
+      } else if (state.board[Cell(neighbor.first, neighbor.second).Lin(game.w)]) {
+        ++filled_neighbor;
+      }
+  }
+  return filled_neighbor;
+}
+
+int calcSumCellPositionY(const vector<pair<int,int> >& current_cell_positions) {
+  int total = 0;
+  for(auto& position : current_cell_positions) {
+    total += position.second;
+  }
+  return total;
+}
+
+int evaluateScore(const Game& game, const State& state, const State& next_state) {
+  // units should go to the place which has many neighbors (number_of_neighbors)
+  vector< vector<pair<int, int> > > directions = {
+    {
+      make_pair(-1,-1), make_pair( 0,-1), make_pair(-1, 0),
+      make_pair( 1, 0), make_pair(-1, 1), make_pair( 0, 1)
+    }, {
+      make_pair( 0,-1), make_pair( 1,-1), make_pair(-1, 0),
+      make_pair( 1, 0), make_pair( 0, 1), make_pair( 1, 1)
+    }
+  };
+
+  // get current status
+  const Unit& unit = game.CurrentUnit(state.source_idx);
+  vector<pair<int,int> > current_cell_positions;
+  for (const auto& cell : unit.cells) {
+    Cell current_cell = cell.Rotate(Cell(0,0), state.rot).TranslateAdd(state.pivot);
+    current_cell_positions.push_back(make_pair(current_cell.x, current_cell.y));
+  }
+
+  // calculate neighbor cells
+  vector<pair<int,int> > neighbors;
+  for (const auto& position : current_cell_positions) {
+    for (const auto& direction : directions[position.second % 2]) {   
+      pair<int, int> neighbor(position.first + direction.first, position.second + direction.second);
+      if (neighbor.second < 0) {
+        continue;
+      } else {
+        neighbors.push_back(neighbor);
+      }
+    }
+  }
+  sort(neighbors.begin(), neighbors.end());
+  auto it = unique(neighbors.begin(), neighbors.end());
+  neighbors.erase(it, neighbors.end());
+  
+  double hole_penalty_base = 1.0;
+  int hole_size_threshold = 1;
+  
+  /*
+  int ave_y = calcSumCellPositionY(current_cell_positions) / current_cell_positions.size();
+  int number_of_neighbors = calculateFilledNeighbors(game, state, neighbors);
+  int distance_from_center = 0; //calculateDistanceFromCenter(game, current_cell_positions);
+  int hole_penalty = 0; //avoidHole(game, state, current_cell_positions, neighbors, directions, hole_penalty_base, 0, hole_size_threshold);
+  return ave_y + next_state.score + number_of_neighbors + distance_from_center + hole_penalty;
+  */
+  
+  int base = 1000000000;
+  double ave_y_ratio = 1.1 * calcSumCellPositionY(current_cell_positions) / (double)(current_cell_positions.size() * game.h);
+  double filled_neighbors_ratio = calculateFilledNeighbors(game, state, neighbors) / (double) neighbors.size();
+  double distance_from_center = 1; //calculateDistanceFromCenter(game, current_cell_positions);
+  double hole_penalty = 1; //avoidHole(game, state, current_cell_positions, neighbors, directions, hole_penalty_base, 1, hole_size_threshold);
+  double point_up_ratio = (next_state.score + 1) / (double) (state.score + 1);
+  return (int)(base * point_up_ratio * ave_y_ratio * filled_neighbors_ratio * distance_from_center * hole_penalty);
+}
 };
 
 
@@ -106,7 +246,7 @@ pair<int, string> SubmarineAI::Step(const Game& game, const State& initial_state
     if (visited[visited_index]) { continue; }
     visited[visited_index] = true;
 
-    const vector<char> commands = {'p', 'b', 'a', 'l', 'd', 'k'};
+    const vector<char> commands = {'!', 'e', 'i', ' ', 'd', 'k'};
     for (auto c : commands) {
       State next_state = item.state;
       const CommandResult result = next_state.Command(game, c);
