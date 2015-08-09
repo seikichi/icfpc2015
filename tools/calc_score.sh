aa5f1df2f4d0834fde85bdb88317ac095cb41c27
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-09 23:59:56 +0900

    fix calc score

--- tools/calc_score.sh
+++ tools/calc_score.sh
@@ -3,11 +3,20 @@
 header=""
 data=""
 
+# 全問題を試す時間は無いので，良さそうなのを選んだ
 for i in 0 1 9 15 23 24; do
-    score=$(./play_icfp2015 -f problems/problem_${i}.json -t 60 2>&1 \
-                | grep '^best_score' \
-                | sed -e 's/best_score: //' \
-                | awk '{m+=$1} END{print int(m/NR);}')
+    # 標準エラー出力に best_score を出すので 2>&1
+    output=$(./play_icfp2015 -f problems/problem_${i}.json -t 60 2>&1)
+
+    if [ $? -ne 0 ]; then
+        echo output 1>&2
+        output="best_score: -1"
+    fi
+
+    score=$(echo "$output" \
+                   | grep '^best_score' \
+                   | sed -e 's/best_score: //' \
+                   | awk '{m+=$1} END{print int(m/NR);}')
     header="${header}problem${i},"
     data="${data}${score},"
 done
