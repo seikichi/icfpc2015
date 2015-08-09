f2ad8116d9e0f5be242760fcdd77a5adecc0df7c
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-09 14:41:35 +0900

    update server

--- tools/server/polling.py
+++ tools/server/polling.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
+import sys
 import re
 import json
 import os
@@ -15,18 +17,26 @@ db = client.icfpc2015
 TEAM_ID = os.environ.get('TEAM_ID', None)
 API_TOKEN = os.environ.get('API_TOKEN', None)
 
+if not API_TOKEN or not TEAM_ID:
+    print('Error: please set API_TOKEN and TEAM_ID to env. variable.')
+    sys.exit(-1)
+
 while True:
     try:
+        print('downloading ...', file=sys.stderr)
         text = requests.get('https://davar.icfpcontest.org/rankings.js').text
+        print('done', file=sys.stderr)
         data = json.loads(re.sub(r'^var\s*data\s*=\s*', r'', text))
-        for setting in data['data']['settings']:
+        for i, setting in enumerate(data['data']['settings']):
+            print(i, file=sys.stderr)
             for d in setting['rankings']:
                 if d['teamId'] != int(TEAM_ID) or len(d['tags']) != 1:
                     continue
                 tag = d['tags'][0]
+                print("update {0}".format(tag), file=sys.stderr)
                 output = {'data': d, 'date': datetime.datetime.utcnow()}
-                db.submits.update({'input.data.tag': tag}, {'$set': {'output': output}}, upsert=False)
-        time.sleep(60 * 1000)
+                db.submits.update({'input.new_tag': tag}, {'$set': {'output': output}}, upsert=False)
+        time.sleep(20 * 1000)
     except:
-        print("Unexpected error:", sys.exc_info()[0])
-        time.sleep(60 * 1000)
+        print("Unexpected error:", sys.exc_info()[0], file=sys.stderr)
+        time.sleep(20 * 1000)
