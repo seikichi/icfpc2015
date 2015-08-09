ce08cf71d8b0964af858a5e6d0884709481f9efa
Yusuke Nojima <nojima@ynojima.com>
2015-08-09 15:47:23 +0900

    Refactor submarineai

--- src/submarineai.cc
+++ src/submarineai.cc
@@ -44,6 +44,82 @@ long long getTime() {
 
 };
 
+
+TleState SubmarineAI::ShouldExitLoop(long long unit_start_time) const {
+  long long now = getTime();
+  if (now - unit_start_time >= time_limit_per_unit)
+    return TleState::Yellow;
+  if (now - game_start_time >= time_limit / 2)
+    return TleState::Red;
+  return TleState::Green;
+}
+
+pair<int, string> SubmarineAI::Step(const Game& game, const State& initial_state, int loop_count) {
+  // 1ユニットごとにループ
+  const int visited_w = 3 * game.w;
+  const int visited_offset_x = game.w;
+  const int visited_h = 3 * game.h;
+  const int visited_offset_y = game.h;
+  vector<bool> visited(visited_h * visited_w * 6, false);
+
+  if (initial_state.IsClear(game) || initial_state.IsGameOver(game)) {
+    return make_pair(-1, "");
+  }
+
+  int max_score = -1;
+  string best_commands = "";
+  priority_queue<Item> Q;
+  Q.push(Item(game, initial_state, ""));
+
+  long long start_time = getTime();
+  while (!Q.empty()) {
+    TleState tle_state = ShouldExitLoop(start_time);
+    if (tle_state == TleState::Yellow && max_score != -1)
+      break;
+    else if (tle_state == TleState::Red)
+      return make_pair(-1, "");
+
+    const Item item = Q.top(); Q.pop();
+
+    assert(0 <= item.state.pivot.x + visited_offset_x);
+    assert(item.state.pivot.x + visited_offset_x < visited_w);
+    assert(0 <= item.state.pivot.y + visited_offset_y);
+    assert(item.state.pivot.y + visited_offset_y < visited_h);
+    const int visited_index =
+      visited_w * 6 * (item.state.pivot.y + visited_offset_y) +
+      6 * (item.state.pivot.x + visited_offset_x) + item.state.rot;
+
+    if (visited[visited_index]) { continue; }
+    visited[visited_index] = true;
+
+    const vector<char> commands = {'p', 'b', 'a', 'l', 'd', 'k'};
+    for (auto c : commands) {
+      State next_state = item.state;
+      const CommandResult result = next_state.Command(game, c);
+      const string next_commands = item.commands + string(1, c);
+
+      assert(result != GAMEOVER && result != CLEAR);
+      if (result == ERROR) {
+        continue;
+      } else if (result == LOCK) {
+        const int score = evaluateScore(game, item.state, next_state);
+        if (score > max_score) {
+          max_score = score;
+          best_commands = next_commands;
+        }
+      } else if (result == MOVE) {
+        Q.push(Item(game, next_state, next_commands));
+      } else {
+        assert(false);
+      }
+    }
+  }
+
+  cerr << "Loop " << loop_count << ": time=" << getTime() - start_time << " usec, total=" << getTime() - game_start_time << " usec" << endl;
+
+  return make_pair(max_score, best_commands);
+}
+
 string SubmarineAI::Run(const Game& game) {
   string solution;
   State state;
@@ -51,70 +127,14 @@ string SubmarineAI::Run(const Game& game) {
 
   cerr << "AI: submarine" << endl;
 
-  long long time_limit = (long long)time_limit_seconds * 1000000;
-  long long time_limit_per_unit = time_limit / (2 * game.num_source_seeds * game.source_seq.size());
-  long long game_start_time = getTime();
+  time_limit = (long long)time_limit_seconds * 1000000;
+  time_limit_per_unit = time_limit / (2 * game.num_source_seeds * game.source_seq.size());
+  game_start_time = getTime();
 
   for (int loop_count = 0; ; ++loop_count) {
-    // 1ユニットごとにループ
-    const int visited_w = 3 * game.w;
-    const int visited_offset_x = game.w;
-    const int visited_h = 3 * game.h;
-    const int visited_offset_y = game.h;
-    vector<bool> visited(visited_h * visited_w * 6, false);
-
-    if (state.IsClear(game) || state.IsGameOver(game)) { break; }
-
-    int max_score = -1;
-    string best_commands = "";
-    priority_queue<Item> Q;
-    Q.push(Item(game, state, ""));
-
-    long long start_time = getTime();
-    for (unsigned loop_count = 0; !Q.empty(); ++loop_count) {
-      long long now = getTime();
-      if (now - start_time >= time_limit_per_unit && max_score != -1)
-          break;
-      if (now - game_start_time >= time_limit / 2)
-          goto finish;
-
-      const Item item = Q.top(); Q.pop();
-
-      assert(0 <= state.pivot.x + visited_offset_x);
-      assert(state.pivot.x + visited_offset_x < visited_w);
-      assert(0 <= state.pivot.y + visited_offset_y);
-      assert(state.pivot.y + visited_offset_y < visited_h);
-      const int visited_index =
-        visited_w * 6 * (item.state.pivot.y + visited_offset_y) +
-        6 * (item.state.pivot.x + visited_offset_x) + item.state.rot;
-
-      if (visited[visited_index]) { continue; }
-      visited[visited_index] = true;
-
-      const vector<char> commands = {'p', 'b', 'a', 'l', 'd', 'k'};
-      for (auto c : commands) {
-        State next_state = item.state;
-        const CommandResult result = next_state.Command(game, c);
-        const string next_commands = item.commands + string(1, c);
-
-        assert(result != GAMEOVER && result != CLEAR);
-        if (result == ERROR) {
-          continue;
-        } else if (result == LOCK) {
-          const int score = evaluateScore(game, item.state, next_state);
-          if (score > max_score) {
-            max_score = score;
-            best_commands = next_commands;
-          }
-        } else if (result == MOVE) {
-          Q.push(Item(game, next_state, next_commands));
-        } else {
-          assert(false);
-        }
-      }
-    }
-
-    cerr << "Loop " << loop_count << ": time=" << getTime() - start_time << " usec, total=" << getTime() - game_start_time << " usec" << endl;
+    auto p = Step(game, state, loop_count);
+    int max_score = p.first;
+    const string& best_commands = p.second;
 
     // modify solution by using best_commands
     if (max_score == -1) { break; }
@@ -122,7 +142,6 @@ string SubmarineAI::Run(const Game& game) {
     solution += best_commands;
   }
 
-finish:
   std::cerr << state.score << endl;
   return solution;
 }
