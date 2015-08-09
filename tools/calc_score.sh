334a99d1adab5474fb27c397f0fe33cf716539e1
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-09 22:58:47 +0900

    update calc score params

--- tools/calc_score.sh
+++ tools/calc_score.sh
@@ -3,8 +3,8 @@
 header=""
 data=""
 
-for i in {0..1}; do
-    score=$(./play_icfp2015 -f problems/problem_${i}.json  2>&1 \
+for i in 0 1 9 15 23 24; do
+    score=$(./play_icfp2015 -f problems/problem_${i}.json -t 60 2>&1 \
                 | grep '^best_score' \
                 | sed -e 's/best_score: //' \
                 | awk '{m+=$1} END{print int(m/NR);}')
