fb03928b2e1ea2a8c015cf49f1d47e5f4261ae58
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-09 15:29:01 +0900

    update submit script

--- tools/solve_and_submit_all.sh
+++ tools/solve_and_submit_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-# Usage: ./submit.sh problem_file_1.json problem_file_2.json ...
+if [ $# -eq 0 ]; then
+    echo 'Usage: solve_and_submit_all.sh problem_file_1.json problem_file_2.json ...'
+    exit 1
+fi
 
 if [ ! -n "${API_TOKEN-}" ]; then
     echo 'please set API_TOKEN env. variable'
@@ -14,11 +17,14 @@ fi
 
 make
 
+DIR=$(dirname $0)
+
 for problem in $@; do
-    OUTPUT=$(./play_icfp2015 -f "$problem")
+    OUTPUT=$(${DIR}/../play_icfp2015 -f "$problem")
+
     curl --user :$API_TOKEN \
          -X POST \
          -H "Content-Type: application/json" \
          -d "$OUTPUT" \
-         https://davar.icfpcontest.org/teams/$TEAM_ID/solutions ; 
+         https://davar.icfpcontest.org/teams/$TEAM_ID/solutions
 done
