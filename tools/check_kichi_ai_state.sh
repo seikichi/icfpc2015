583103f3f3fa2e769d823cab22894f9296e609c0
seikichi <seikichi@kmc.gr.jp>
2015-08-10 08:52:57 +0000

    fix kichi ai state checker

--- tools/check_kichi_ai_state.sh
+++ tools/check_kichi_ai_state.sh
@@ -2,6 +2,6 @@
 
 make
 
-OUTPUT=$(AI=kichiai ./play_icfp2015 -f ./problems/problem_0.json 2>&1 1>/dev/null)
+OUTPUT=$(AI=kichiai ./play_icfp2015 -f ./problems/problem_0.json 2>&1 1>/dev/null | grep -v  'problem_id')
 
 diff ./misc/kichiai_problem_0_stderr_20150810_1537_in_ec2.txt <(echo "$OUTPUT")
