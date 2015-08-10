c8363b47341178a8fd7cef51e5eddeafc75e4033
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-10 16:47:14 +0900

    adds score utility

---
+++ tools/calc_score_with_evaluator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+header=""
+data=""
+
+for e in qwerty kichi; do
+    # 標準エラー出力に best_score を出すので 2>&1
+    output=$(EVALUATOR=${e} ./play_icfp2015 -f problems/problem_${ID}.json -t ${TIMEOUT:-60} 2>&1)
+    echo "$output" 1>&2
+
+    if [ $? -ne 0 ]; then
+        output="best_score: -1"
+    fi
+
+    score=$(echo "$output" \
+                   | grep '^best_score' \
+                   | sed -e 's/best_score: //' \
+                   | awk '{m+=$1} END{print int(m/NR);}')
+    header="${header}${e}_problem${ID},"
+    data="${data}${score},"
+done
+
+echo $header
+echo $data
---
+++ tools/score_all_for_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+export AI=submarineai
+
+export TIMEOUT=30
+./tools/calc_score.sh > submarineai_20_output.csv
+
+export TIMEOUT=60
+./tools/calc_score.sh > submarineai_60_output.csv
+
+export TIMEOUT=30
+
+export ID=0
+./tools/calc_score_with_evaluator.sh > evalustor_0_output.csv
+
+export ID=1
+./tools/calc_score_with_evaluator.sh > evalustor_1_output.csv
+
+export ID=24
+./tools/calc_score_with_evaluator.sh > evalustor_24_output.csv
