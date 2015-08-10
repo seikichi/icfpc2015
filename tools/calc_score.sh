0069e69ab90179b04153643860d577e9af611b82
Seiichi KONDO <seikichi@kmc.gr.jp>
2015-08-10 10:57:20 +0900

    update calc score so that it can use TIMEOUT env. variable

--- tools/calc_score.sh
+++ tools/calc_score.sh
@@ -6,7 +6,7 @@ data=""
 # 全問題を試す時間は無いので，良さそうなのを選んだ
 for i in 0 1 9 15 23 24; do
     # 標準エラー出力に best_score を出すので 2>&1
-    output=$(./play_icfp2015 -f problems/problem_${i}.json -t 60 2>&1)
+    output=$(./play_icfp2015 -f problems/problem_${i}.json -t ${TIMEOUT:-60} 2>&1)
     echo "$output" 1>&2
 
     if [ $? -ne 0 ]; then
